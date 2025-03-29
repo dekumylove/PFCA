d3af36134ba97be9617ad71971065098ca874333
dekumylove <60187278+dekumylove@users.noreply.github.com>
2025-03-29 22:27:29 +0800

    Update PKGAT.py

--- src/model/PKGAT.py
+++ src/model/PKGAT.py
@@ -31,7 +31,8 @@ class GraphAttentionLayer(nn.Module):
         alpha,
         hidden_dim,
         threshold,
-        nrelation=12,
+        K = 3,
+        nrelation=9,
         device_id=1,
         device=torch.device("cuda"),
     ):
@@ -41,11 +42,12 @@ class GraphAttentionLayer(nn.Module):
 
         for i in range(nrelation):
             if i != 0:
-                self.W.append(nn.Parameter(torch.zeros(size=(hidden_dim, hidden_dim))))
+                self.W.append(nn.Parameter(torch.randn(size=(hidden_dim, hidden_dim))))
                 nn.init.xavier_uniform_(self.W[-1].data, gain=1.414)
             else:
                 self.W.append(nn.Parameter(torch.ones(size=(hidden_dim, hidden_dim)), requires_grad = False))   # padding matrixs
 
+        self.alpha = torch.randn(K)
         self.linear = nn.Linear(2 * noutfeat, 1, bias=False)
         self.dropout = nn.Dropout(p=dropout)
         self.leakyrelu = nn.LeakyReLU(alpha)
@@ -53,69 +55,74 @@ class GraphAttentionLayer(nn.Module):
         self.num_rel = nrelation
         self.hidden_dim = hidden_dim
         self.num_feat = ninfeat
-        self.W_p = nn.Parameter(torch.ones(hidden_dim+noutfeat))
+        self.W_p = nn.Parameter(torch.ones(hidden_dim * 2)) # hidden_dim + embed_dim
         self.b_p = nn.Parameter(torch.ones(1))
         self.sigmoid = nn.Sigmoid()
         self.threshold = threshold
         self.device_id = device_id
         self.softmax = nn.Softmax(dim = -1)
-        self.causal_threshold = nn.Parameter(torch.tensor(0.5))
-        self.path_threshold = nn.Parameter(torch.tensor(0.01))
-        self.causal_mlp = nn.Linear(hidden_dim + noutfeat, 1)
-        self.path_mlp = nn.Linear(hidden_dim, 1)
+        self.causal_mlp = nn.Linear(hidden_dim * 2, 1) # hidden_dim + embed_dim
         self.MLP = nn.Sequential(
             nn.Linear(hidden_dim, 1)
         )
 
-    def path_calculation_filtering(self, path_index, path_structure, path_target, feature_embed, h_time, path_filtering):
+    def path_calculation_filtering(self, path_index, path_node_embed, path_relation, h_time, path_filtering):
         """
         This function is used to calculate the path messages and filter the paths.
         :param path_index: extracted paths in the personalized knowledge graphs (PKGs)
-        :param path_index: the structure of the extracted paths
+        :param path_node: the nodes in the extracted paths
+        :param path_relation: the relations in the extracted paths
         :param path_target: targets connected to each path
-        :param feature_embed: feature embeddings tensor(bs, nv, mf, embed_dim)
         :param h_time: patient hidden states obtained from the time-series module
         """
         params = torch.stack([param for param in self.W], dim = 0)
         bs, nv, mf, mp = path_index.size()
-        _, np, K, nr = path_structure.size()
+        _, np, K, nr = path_relation.size()
+        _, np, K, hd = path_node_embed.size()
 
         # path calculation
-        structures = path_structure.view(-1, nr)
+        path_relation = path_relation.view(-1, nr)
         params = params.view(nr, -1)
-        structures = torch.mm(structures, params)
-        structures = structures.view(bs, nv, mf, mp, K, self.hidden_dim, self.hidden_dim)
-        M_j = torch.prod(structures, dim=-3)  # tensor(bs, nv, mf, mp, self.hidden_dim, self.hidden_dim)
-        feat_embed_1 = feature_embed.unsqueeze(dim=-2).repeat(1, 1, 1, mp, 1)
-        M_j = torch.einsum('bvfpdd, bvfpd -> bvfpd', M_j, feat_embed_1)
+        path_relation = torch.matmul(path_relation, params) # bs*np*K*hd*hd
+        path_relation = path_relation.view(bs, nv, mf, mp, K, self.hidden_dim, self.hidden_dim)
+        path_node_embed = path_node_embed.view(bs, nv, mf, mp, K, hd)
+
+        cumulative = path_node_embed[..., 0, :].clone()  # Shape: [bs, nv, mf, mp, hd]
+        for k in range(K):
+            relation = path_relation[..., k, :, :]  # Relation at step k: [bs, nv, mf, mp, hd, hd]
+            alpha_k = self.alpha[k]   
+            cumulative = torch.matmul(cumulative.unsqueeze(-2), relation).squeeze(-2)
+            if k == K - 1:
+                cumulative = cumulative * alpha_k
+            else:
+                cumulative = cumulative * alpha_k + path_node_embed[..., k+1, :]
 
         # path filtering
         h_time_1 = h_time.unsqueeze(dim=-2).unsqueeze(dim=-2).unsqueeze(dim=-2).repeat(1, nv, mf, mp, 1)
-        msg = torch.cat([h_time_1, M_j], dim=-1)
-        msg = torch.einsum('bvfpa, a -> bvfp', msg, self.W_p)
-        mask = F.gumbel_softmax(msg, hard=True)
-        path_score = torch.einsum('bvfp, bvfpd -> bvfpd', mask, M_j)
-        path_attn = torch.sigmoid(self.path_mlp(path_score)).squeeze(dim=-1)
+        msg = torch.cat([h_time_1, cumulative], dim=-1)
+        path_attn = torch.einsum('bvfpa, a -> bvfp', msg, self.W_p)
+        path_attn = torch.sigmoid(path_attn)
+        path_attn = F.gumbel_softmax(path_attn, hard=True)
+        path_attn = torch.where(path_attn == 0, float('1e-6'), path_attn)
+        path_attn = self.softmax(path_attn)
+        
         if path_filtering:
-            M_j = torch.einsum('bvfp, bvfpd -> bvfpd', path_attn, M_j)
-        path_target = path_target.view(bs, nv, mf, mp, -1)
-        M_j = torch.einsum('bvfpt, bvfpd -> bvftd', path_target, M_j)
+            cumulative = torch.einsum('bvfp, bvfpd -> bvfd', path_attn, cumulative)
 
-        return M_j, path_attn
+        return cumulative, path_attn
     
     def joint_impact(self, M_j):
         """
         This function is used to calculate the joint impact of patient features.
         :param M_j: messages transmitted through paths
         """
-        bs, nv, mf, nt, hd = M_j.size()
-        M_j = M_j.permute(0, 1, 3, 2, 4)
+        bs, nv, mf, hd = M_j.size()
         normalized_embeddings = F.normalize(M_j, p=2, dim=-1)
         normalized_embeddings = normalized_embeddings.reshape(-1, mf, self.hidden_dim)
         normalized_embeddings_2 = normalized_embeddings.reshape(-1, self.hidden_dim, mf)
         cosine_similarity = torch.bmm(normalized_embeddings, normalized_embeddings_2)
-        cosine_similarity = cosine_similarity.view(bs, nv, nt, mf, mf)
-        M_j = torch.einsum('bvtff, bvtfd -> bvtfd', cosine_similarity, M_j)
+        cosine_similarity = cosine_similarity.view(bs, nv, mf, mf)
+        M_j = torch.einsum('bvff, bvfd -> bvfd', cosine_similarity, M_j)
 
         return M_j
     
@@ -125,21 +132,18 @@ class GraphAttentionLayer(nn.Module):
         :param M_j: messages transmitted through paths
         :param h_time: patient hidden states obtained from the time-series module
         """
-        bs, nv, nt, mf, hd = M_j.size()
-        h_time_2 = h_time.unsqueeze(dim=-2).unsqueeze(dim=-2).unsqueeze(dim=-2).repeat(1, nv, nt, mf, 1)
+        bs, nv, mf, hd = M_j.size()
+        h_time_2 = h_time.unsqueeze(dim=-2).unsqueeze(dim=-2).repeat(1, nv, mf, 1)
         attn_scores = self.causal_mlp(torch.cat([h_time_2, M_j], dim=-1)).squeeze(dim=-1)
-        attn_causal = torch.sigmoid(attn_scores - self.causal_threshold)
+        attn_causal = torch.sigmoid(attn_scores)
         attn_trivial = 1 - attn_causal
-        g_c = torch.einsum('bvtf, bvtfd -> bvtd', attn_causal, M_j)
-        g_c = self.MLP(g_c).squeeze(dim = -1)
-        g_t = torch.einsum('bvtf, bvtfd -> bvtd', attn_trivial, M_j)
-        g_t = self.MLP(g_t).squeeze(dim = -1)
+        g_c = torch.einsum('bvf, bvfd -> bvd', attn_causal, M_j)
+        g_t = torch.einsum('bvf, bvfd -> bvd', attn_trivial, M_j)
         g_i = g_c.clone()
 
         if intervention == "random_sample":
             M = g_c.size(0)
             II = torch.randint(0, M, (M,), device=g_c.device)
-            II = II + (II == torch.arange(M, device=g_c.device)).int()
             g_i += g_t[II]
         elif intervention == "random_visit":
             M = g_c.size(0)
@@ -149,7 +153,7 @@ class GraphAttentionLayer(nn.Module):
 
         return g_i, g_c, g_t, attn_causal
 
-    def forward(self, feature_embed, h_time, path_index, path_structure, path_target, path_filtering, joint_impact, causal_analysis, intervention):
+    def forward(self, h_time, path_index, path_node_embed, path_relation, path_filtering, joint_impact, causal_analysis, intervention):
         """
         :param path_index: extracted paths in the personalized knowledge graphs (PKGs)
         :param path_index: the structure of the extracted paths
@@ -159,7 +163,7 @@ class GraphAttentionLayer(nn.Module):
         """
 
         # path calculation and filtering
-        M_j, path_attn = self.path_calculation_filtering(path_index = path_index, path_structure = path_structure, path_target = path_target, feature_embed = feature_embed, h_time = h_time, path_filtering = path_filtering)
+        M_j, path_attn = self.path_calculation_filtering(path_index = path_index, path_node_embed = path_node_embed, path_relation = path_relation, h_time = h_time, path_filtering = path_filtering)
 
         # joint impact
         if joint_impact:
@@ -184,8 +188,9 @@ class GATModel(nn.Module):
         gat_hid,
         dropout,
         threshold,
+        K = 3,
         alpha=0.2,
-        nrelation=12,
+        nrelation=9,
         device_id=1,
         device=torch.device("cuda"),
     ):
@@ -207,15 +212,14 @@ class GATModel(nn.Module):
             device=self.device,
             hidden_dim=self.hidden_dim,
             threshold=threshold,
-            device_id=device_id
+            device_id=device_id,
+            K = K
         )
         self.output_layer = nn.Sequential(
-            nn.Linear(gat_hid, gat_hid),
-            nn.Dropout(p=dropout),
             nn.Sigmoid()
         )
 
-    def forward(self, feat_index, path_index, path_structure, path_target, h_time, path_filtering, joint_impact, causal_analysis, intervention = "trivial_mean"):
+    def forward(self, feat_index, path_index, path_node, path_relation, h_time, path_filtering, joint_impact, causal_analysis, intervention = "trivial_mean"):
         """
         :param rel_index: extracted paths in the personalized knowledge graphs (PKGs)
         :param feat_index: patient feature index
@@ -223,22 +227,21 @@ class GATModel(nn.Module):
         """
         # 1. feature embeddings
         embeds = self.embedding.embedding.weight
-        feature_embed = torch.einsum('bvmn, nd -> bvmd', feat_index, embeds)
+        # print(f'embeds: {embeds}')
+        path_node_embed = torch.einsum('bpkf, fd -> bpkd', path_node, embeds)
 
         # 2. Graph Attention Layers
         if causal_analysis:
             g_i, g_c, g_t, path_attentions, causal_attentions = self.pkgat(
-                feature_embed=feature_embed,
                 h_time=h_time,
                 path_index=path_index,
-                path_structure=path_structure,
-                path_target = path_target,
+                path_node_embed=path_node_embed,
+                path_relation=path_relation,
                 path_filtering=path_filtering,
                 joint_impact=joint_impact,
                 causal_analysis=causal_analysis,
                 intervention=intervention
             )  # (batch_size, num_visit, hidden_dim)
-
             g_i = self.output_layer(g_i)
             g_c = self.output_layer(g_c)
             g_t = self.output_layer(g_t)
@@ -251,10 +254,10 @@ class GATModel(nn.Module):
             return g_i, g_c, g_t, path_attentions, causal_attentions
         else:
             g_kg = self.self.pkgat(
-                feature_embed=feature_embed,
                 h_time=h_time,
                 path_index=path_index,
-                path_structure=path_structure,
+                path_node=path_node,
+                path_relation=path_relation,
                 path_filtering=path_filtering,
                 joint_impact=joint_impact,
                 causal_analysis=causal_analysis,
